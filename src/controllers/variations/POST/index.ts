046559ab9a3e13ba7e6581b530ad7c5eecba1dda
alexisfrene <alexisfrene@gmail.com>
2024-06-27 09:48:44 -0300

    refactor in multer

--- src/controllers/variations/POST/index.ts
+++ src/controllers/variations/POST/index.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
-import db from "../../../lib/sequelize";
 import { v4 as uuidv4 } from "uuid";
+import db from "../../../lib/sequelize";
 import { uploadToCloudinary } from "../../../lib";
 
 const User = db.User;

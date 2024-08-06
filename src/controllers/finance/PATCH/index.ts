04dd873917f13b3e775d272c3f4bcce67fae82a7
alexisfrene <alexisfrene@gmail.com>
2024-08-06 15:49:51 -0300

    add create financial account

---
+++ src/controllers/finance/PATCH/index.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import { v4 as uuidv4 } from "uuid";
+import db from "../../../lib/sequelize";
+
+const FinancialAccounts = db.FinancialAccounts;
+
+export const addFinancialAccount = async (req: Request, res: Response) => {
+  const { financial_accounts_id, value, user_id } = req.body;
+  try {
+    if (!financial_accounts_id)
+      res.status(400).json({
+        message: "Error al agregar una cuenta financiera",
+        error: true,
+      });
+    const financialAccount = await FinancialAccounts.findByPk(
+      financial_accounts_id
+    );
+    await financialAccount.update({
+      values: [
+        {
+          name: value.name,
+          value: value.value,
+          updatedAt: new Date(),
+          createdAt: new Date(),
+          expiration: value?.expiration || 0,
+          id: uuidv4(),
+        },
+      ],
+    });
+    return res.status(200).json({ financial_accounts_id, value, user_id });
+  } catch (error) {
+    return res.status(501).json({ message: error });
+  }
+};
--- src/controllers/finance/POST/index.ts
+++ src/controllers/finance/POST/index.ts
@@ -14,17 +14,24 @@ export const createMovement = async (req: Request, res: Response) => {
 };
 
 export const createFinancialAccounts = async (req: Request, res: Response) => {
-  const { name, type, values, user_id } = req.body;
-  console.log("fdasdf");
+  const { name, type, user_id } = req.body;
+
   try {
-    const newFinancialAccounts = await FinancialAccounts.create({
-      name,
-      type,
-      values,
-      user_id,
-    });
-    return res.status(200).json(newFinancialAccounts);
+    if (type === "inflow_of_money" || type === "money_outflow") {
+      const newFinancialAccounts = await FinancialAccounts.create({
+        name,
+        type,
+        user_id,
+      });
+      return res.status(200).json(newFinancialAccounts);
+    } else {
+      return res
+        .status(400)
+        .json({ message: "Error al crear una cuenta financiera", error: true });
+    }
   } catch (error) {
-    return res.status(501).json({ message: error });
+    return res
+      .status(400)
+      .json({ message: "Error al crear una cuenta financiera", error: true });
   }
 };

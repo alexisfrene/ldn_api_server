d66e1e1262385adf8a11a6323a996a4e35046a22
alexisfrene <alexisfrene@gmail.com>
2024-09-26 18:07:05 -0300

    refactor in controllers

--- src/routes/categories/categoriesRoutes.ts
+++ src/routes/categories/categoriesRoutes.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { matchedData, query, validationResult } from "express-validator";
 import {
   addCategoryValue,
   createCategories,
@@ -15,15 +16,22 @@ import { validateCategoryQuery } from "../../middleware";
 
 const router = express.Router();
 
-router.get("/:id", validateCategoryQuery, async (req, res) => {
-  const { type } = req.query;
+router.get(
+  "/:id",
+  query("type").notEmpty().escape(),
+  validateCategoryQuery,
+  async (req, res) => {
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+      const data = matchedData(req);
+      if (data.type === "collection") return getByIdCategory(req, res);
+      if (data.type === "value") return getByIdCategoryValue(req, res);
+      if (data.type === "icon") return getByIdValueImageURL(req, res);
+    }
 
-  if (type === "collection") return getByIdCategory(req, res);
-  if (type === "value") return getByIdCategoryValue(req, res);
-  if (type === "icon") return getByIdValueImageURL(req, res);
-
-  return res.status(400).json({ error: true, message: "Tipo inválido" });
-});
+    return res.status(400).json({ errors: result.array() });
+  }
+);
 router.get("/", getAllCategories);
 router.post("/", upload.array("files"), createCategories);
 router.patch("/:id", upload.array("files"), async (req, res) => {

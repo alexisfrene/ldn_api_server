5e76975d8599c0f0ab0788aa0f227dafc4c4bdd6
alexisfrene <alexisfrene@gmail.com>
2024-10-08 21:47:19 -0300

    ggffg

--- src/routes/users/usersRoutes.ts
+++ src/routes/users/usersRoutes.ts
@@ -3,20 +3,15 @@ import {
   getAvatar,
   getPreferenceInProductView,
   preferenceInProductView,
+  changeAvatar,
 } from "../../controllers";
-import { changeAvatar } from "../../controllers/users/PATCH";
 import { upload } from "../../lib";
 
 const router = express.Router();
 
 router.get("/preference", getPreferenceInProductView);
 router.get("/avatar", getAvatar);
-router.patch(
-  "/avatar",
-  upload.single("file"),
-
-  changeAvatar
-);
+router.patch("/avatar", upload.single("file"), changeAvatar);
 router.put("/preference", preferenceInProductView);
 
 export default router;

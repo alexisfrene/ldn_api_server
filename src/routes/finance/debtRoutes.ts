329fe564f09fb92617dd9d9f36d82ba4e3c9d847
alexisfrene <alexisfrene@gmail.com>
2024-11-05 00:39:17 -0300

    add validation in finance

--- src/routes/finance/debtRoutes.ts
+++ src/routes/finance/debtRoutes.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { createDebts, getAllDebts, getDebtsById } from "@controllers";
+import { runValidate } from "@middlewares";
+import { createDebtValidations } from "@validators";
 
 const router = express.Router();
 router.get("/", getAllDebts);
 router.get("/:id", getDebtsById);
-router.post("/", createDebts);
+router.post("/", runValidate(createDebtValidations), createDebts);
 
 export default router;

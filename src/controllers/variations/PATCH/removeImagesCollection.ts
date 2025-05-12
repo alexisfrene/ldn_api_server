ddfbdb7bab0cb1c2f2f36bef57a8ececf805dd7a
Alexis <alexisfrene@gmail.com>
2025-05-12 18:30:06 -0300

    fix(minio): correct object name formatting in deleteFromMinio function

--- src/controllers/variations/PATCH/removeImagesCollection.ts
+++ src/controllers/variations/PATCH/removeImagesCollection.ts
@@ -10,33 +10,37 @@ export const removeImagesCollection = async (req: Request, res: Response) => {
   const { public_id } = req.body;
   const user_id = req.user;
 
-  if (!user_id)
-    return res.status(401).json({ error: true, message: "No autorizado" });
   if (!public_id)
     return res
       .status(400)
       .json({ error: true, message: "No se paso nada en el cuerpo" });
+
   if (!variationId)
     return res.status(400).json({ error: true, message: "No autenticado" });
+
   const variation = await Variation.findByPk(variationId);
   if (!variation)
     return res.status(404).json({
       error: true,
       message: "No se encontró la variación seleccionada",
     });
+
   const collectionSelected = variation.values.find(
     (value: { id: string }) => value.id === collection_id,
   );
+
   if (!collectionSelected)
     return res
       .status(404)
       .json({ error: true, message: "No se encontró la collection" });
+
   const values = variation.values.filter(
     (value: { id: string }) => value.id !== collectionSelected.id,
   );
+
   await deleteFromMinio(public_id, `${user_id}/variations`);
-  const newValues = collectionSelected.images.filter(
-    (i: string) => i !== public_id,
+  const newValues = collectionSelected.images.filter((i: string) =>
+    i.includes(public_id) ? false : i,
   );
 
   const updateVariation = await variation.update({

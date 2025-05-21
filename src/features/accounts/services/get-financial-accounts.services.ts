acd5a5e4a594843108386a184dbf61539dda61c1
Alexis <alexisfrene@gmail.com>
2025-05-21 00:28:30 -0300

    feat(accounts): implement pagination for financial accounts retrieval

--- src/features/accounts/services/get-financial-accounts.services.ts
+++ src/features/accounts/services/get-financial-accounts.services.ts
@@ -1,58 +1,60 @@
 import { Uuid } from "types";
 import { models } from "@lib/sequelize";
 
-const { FinancialAccount, User, PaymentMethod } = models;
+const { FinancialAccount, PaymentMethod } = models;
 
-export const getFinancialAccountsService = async (user_id: Uuid) => {
-  const user = await User.findByPk(user_id);
-  const financialAccounts = user ? await user.getUserFinancialAccounts() : [];
-  const movements = user ? await user.getUserMovements() : [];
+export const getFinancialAccountsService = async (
+  user_id: Uuid,
+  page: number,
+  limit: number,
+) => {
+  const offset = (page - 1) * limit;
 
-  const formatter = await Promise.all(
-    financialAccounts.map(async (account) => {
-      const financialAccountWithPaymentMethods = await FinancialAccount.findOne(
-        {
-          where: { financial_accounts_id: account.financial_accounts_id },
-          include: [
-            {
-              model: PaymentMethod,
-              attributes: ["payment_method_id", "name"],
-            },
-          ],
-          attributes: [],
-          raw: false,
-        },
-      );
+  const { count, rows } = await FinancialAccount.findAndCountAll({
+    where: { user_id },
+    attributes: ["financial_accounts_id", "name"],
+    distinct: true,
+    include: [
+      {
+        model: PaymentMethod,
+        attributes: ["payment_method_id", "name"],
+      },
+    ],
+    order: [
+      ["name", "ASC"],
+      ["financial_accounts_id", "DESC"],
+    ],
+    limit,
+    offset,
+  });
 
-      const filterMovements = movements.filter(
-        (movement: any) =>
-          movement.financial_accounts_id === account.financial_accounts_id,
-      );
+  const formatted = await Promise.all(
+    rows.map(async (account) => {
+      const movements = await account.getFinancialAccountMovements();
 
-      const totalValue = filterMovements.reduce(
-        (acc: number, movement: any) => {
-          if (movement.type === "money_outflow") {
-            return acc - movement.value;
-          } else {
-            return acc + movement.value;
-          }
-        },
-        0,
-      );
+      const totalValue = movements.reduce((acc: number, movement) => {
+        return movement.type === "money_outflow"
+          ? acc - movement.value
+          : acc + movement.value;
+      }, 0);
 
       return {
         financial_accounts_id: account.financial_accounts_id,
         total: totalValue,
         name: account.name,
-        paymentMethods: financialAccountWithPaymentMethods?.PaymentMethods?.map(
-          (paymentMethod) => ({
-            name: paymentMethod.name,
-            payment_method_id: paymentMethod.payment_method_id,
-          }),
-        ),
+        paymentMethods: account.PaymentMethods,
       };
     }),
   );
 
-  return formatter.sort((a, b) => b.total - a.total);
+  return {
+    status: 200,
+    body: {
+      accounts: formatted,
+      totalPages: Math.ceil(count / limit),
+      currentPage: page,
+      totalItems: count,
+      limit,
+    },
+  };
 };
